0155fdd2da1ba7e7fa6fd08bc579c887014fdf13
badbuy <14489146+badguy001@user.noreply.gitee.com>
2025-03-09 09:58:30 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <script setup>
-
-import {ref} from "vue";
-
-const params = new URLSearchParams(window.location.search);
-const name = params.get("uuu");
-
-const uuu = ref("http://acrh17-gz.v6.rocks:60123/s/" + name)
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-<!--  <header>
+  <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld :msg=name />
+      <HelloWorld msg="You did it!" />
     </div>
-  </header>-->
-  {{uuu}}
-  <iframe :src=name style="height: 98vh; width: 200vh; position: fixed; place-items: flex-start"/>
-<!--  <main >
-&lt;!&ndash;    <TheWelcome :msg="name"/>&ndash;&gt;
-  </main>-->
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
 </template>
 
 <style scoped>
@@ -33,4 +27,21 @@ header {
   margin: 0 auto 2rem;
 }
 
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
 </style>
