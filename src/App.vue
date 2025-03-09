108260b9ff66e9ebdc66dfee2d50e9827a4ba4e2
badbuy <14489146+badguy001@user.noreply.gitee.com>
2025-03-09 09:27:50 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+import {ref} from "vue";
+
+const params = new URLSearchParams(window.location.search);
+const name = params.get("uuu");
+
+const uuu = ref("http://acrh17-gz.v6.rocks:60123/s/" + name)
+</script>
+
+<template>
+<!--  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld :msg=name />
+    </div>
+  </header>-->
+  {{uuu}}
+  <iframe :src=name style="height: 98vh; width: 200vh; position: fixed; place-items: flex-start"/>
+<!--  <main >
+&lt;!&ndash;    <TheWelcome :msg="name"/>&ndash;&gt;
+  </main>-->
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <iframe src="http://acrh17-gz.v6.rocks:60123/s/ss3Nqfe"/>
+</template>
